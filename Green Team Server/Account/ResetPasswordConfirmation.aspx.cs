86c1a57e8c27e0afcf83169afd2449434c783f22
TheBicPen <mashkal2000@gmail.com>
2018-04-06 11:21:14 -0700

    commit.

---
+++ Green Team Server/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Green_Team_Server.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Green Team Server/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Green_Team_Server.Startup))]
+namespace Green_Team_Server
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
